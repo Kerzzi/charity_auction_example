edbf52bddeece0bec46e339fdc4138faea4557b2
tamatsyk <tamatsyk@gmail.com>
2015-11-07 22:27:02 +0200

    cover auction with unit tests

---
+++ app/models/auction.rb
@@ -0,0 +1,24 @@
+class Auction < ActiveRecord::Base
+  validate :ends_at_after_starts_at
+  validate :donation_window_ends_at_after_starts_at
+  validates :time_zone_id, inclusion: { in: ActiveSupport::TimeZone.all.map { |tz| tz.tzinfo.identifier } }, allow_blank: true
+
+  private 
+  def donation_window_ends_at_after_starts_at
+    return unless starts_at && donation_window_ends_at
+
+    if starts_at <= donation_window_ends_at
+      errors.add :starts_at, "must be greater than donation_window_ends_at"
+    end
+  end
+
+  def ends_at_after_starts_at
+    return unless starts_at && ends_at
+
+    if ends_at <= starts_at
+      errors.add :ends_at, "must be greater than starts_at"
+    end
+  end
+
+
+end
---
+++ spec/factories/auctions.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :auction do
+    starts_at "2015-11-07 21:49:27"
+ends_at "2015-11-07 21:49:27"
+time_zone_id "MyString"
+physical_address "MyText"
+name "MyString"
+donation_window_ends_at "2015-11-07 21:49:27"
+  end
+
+end
