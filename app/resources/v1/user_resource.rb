5a5db556d8aef40d17d2bc20563f7576d0944b21
tamatsyk <tamatsyk@gmail.com>
2015-11-14 14:42:24 +0200

    describe user resource in json api

--- app/resources/v1/user_resource.rb
+++ app/resources/v1/user_resource.rb
@@ -1,4 +1,17 @@
 module V1
   class UserResource < BaseResource
+
+    class << self
+      def createable_fields(context)
+        [:name,:mobile_phone_number,:email_address,:physical_address]
+      end
+
+      alias_method :updatable_fields, :createable_fields
+
+    end
+
+    def fetchable_fields
+      super + self.class.createable_fields(context) + [:created_at, :updated_at]
+    end
   end
 end
--- spec/resources/v1/user_resource_spec.rb
+++ spec/resources/v1/user_resource_spec.rb
@@ -1,5 +1,23 @@
 module V1
   RSpec.describe UserResource do
+    let :createable_fields do
+      [:name,:mobile_phone_number,:email_address,:physical_address].sort
+    end
 
+    subject do
+      described_class.new User.new, {}
+    end
+
+    it "has the expected createable fields" do
+      expect(described_class.createable_fields({}).sort).to eq createable_fields
+    end
+
+    it "has the expected updatable fields" do
+      expect(described_class.updatable_fields({}).sort).to eq createable_fields
+    end
+
+    it "has the expected fetchable fields" do
+      expect(subject.fetchable_fields.sort).to eq (createable_fields + [:id,:created_at,:updated_at]).sort
+    end
   end
 end
